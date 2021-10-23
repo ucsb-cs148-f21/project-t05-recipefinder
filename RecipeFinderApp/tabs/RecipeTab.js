79f24f8029f87e858d96196dda9b861fbf1f3da7
Richard Rao <richardrao@umail.ucsb.edu>
2021-10-23 14:06:53 -0700

    Separate Bottom Tabs created for ingredients/recipes

---
+++ RecipeFinderApp/tabs/RecipeTab.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+const RecipeTab = () =>
+
+  <View>
+   <Text>Recipes!</Text>
+  </View>
+
+export default RecipeTab;
