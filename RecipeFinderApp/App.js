02c27a8224de7e2a7e12d4474a4ea896041a08dd
Richard Rao <richardrao@umail.ucsb.edu>
2021-10-23 19:25:03 -0700

    Set tabBarIcons for both tabs - maybe need to switch workflow for android?

--- RecipeFinderApp/App.js
+++ RecipeFinderApp/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import IngredientsTab from '../RecipeFinderApp/tabs/IngredientsTab'
 import RecipeTab from '../RecipeFinderApp/tabs/RecipeTab'
@@ -11,9 +12,28 @@ export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <BottomTabs.Navigator>
-          <BottomTabs.Screen name = "Ingredients" component={IngredientsTab} />
-          <BottomTabs.Screen name = "Recipes" component={RecipeTab} />
+        <BottomTabs.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if(route.name === 'Ingredients') {
+                iconName = focused 
+                 ? 'ios-nutrition' 
+                 : 'ios-nutrition-outline';
+              }else if(route.name === 'Recipes') {
+                iconName = focused ? 'ios-book' : 'ios-book-outline';
+              }
+              return  <Ionicons name={iconName} size={size} color={color} />
+            },
+          })}
+          tabBarOption={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+            >
+          <BottomTabs.Screen name="Ingredients" component={IngredientsTab} />
+          <BottomTabs.Screen name="Recipes" component={RecipeTab} />
         </BottomTabs.Navigator>
       </NavigationContainer>
     );
