de8c6d29a127d494faca1da7bd628cb0f68bc9c3
LeonFeng0325 <59664884+LeonFeng0325@users.noreply.github.com>
2021-11-22 14:50:50 -0800

    implement login and authentication

--- RecipeFinderApp/tabs/SignInScreen.js
+++ RecipeFinderApp/tabs/SignInScreen.js
@@ -8,7 +8,8 @@ import {
     TouchableOpacity,
     textInput,
     Platform,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native';
 
 import * as Animatable from 'react-native-animatable';
@@ -16,27 +17,42 @@ import { LinearGradient } from 'expo-linear-gradient';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { AuthContext } from '../component/context';
+import Users from '../Model/users';
 
-const SignInScreen = () => {
+const SignInScreen = ({navigation}) => {
+    const {signIn} = React.useContext(AuthContext); 
 
     const [data, setData] = React.useState({
-        email: "",
+        username: "",
         password: "", 
-        secureTextEntry: true
+        secureTextEntry: true, 
+        isValidUser: true,
+        isValidPassword: true
     });
     
     const textInputChange = (val) =>{
         setData({
             ...data, 
-            email: val
+            username: val
         }); 
     }
 
     const handlePasswordChange = (val) => {
-        setData({
-            ...data, 
-            password: val
-        }); 
+        if (val.trim().length >=8){
+            setData({
+                ...data, 
+                password: val, 
+                isValidPassword: true
+            });
+        } else {
+            setData({
+                ...data, 
+                password: val,
+                isValidPassword: false 
+            }); 
+        }
     }
 
     const updateSecurityTextEntry =() =>{
@@ -44,16 +60,55 @@ const SignInScreen = () => {
             ...data,
             secureTextEntry: !data.secureTextEntry
         });
-        console.log("click")
+    }
+
+    const handleValidUser = (val) => {
+        if( val.trim().length >= 4 ) {
+            setData({
+                ...data,
+                isValidUser: true
+            });
+        } else {
+            setData({
+                ...data,
+                isValidUser: false
+            });
+        }
+    }
+
+
+    const loginHandle = (username, password) => {
+        if (data.username.length == 0 || data.password.length == 0){
+            Alert.alert('Oops!', 'username or password field cannot be empty.', [
+                {text: 'Okay'}
+            ]);
+            return;
+        }
+
+        let founduser = Users.filter(element => {
+            return username == element.username && password== element.password
+        })
+
+        if (founduser.length == 0 ){
+            Alert.alert('Oops!', 'Wrong username or password. Please try again.', [
+                {text: 'Okay'}
+            ]);
+            return;
+        }
+
+        signIn(founduser); 
     }
 
     return (
         <View style = {styles.container}>
+            <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 1}}>
             <View style = {styles.header}>
-                <Text style={styles.text_header}>Welcome to recipe finder!</Text>
+                <Animatable.View animation="tada" duration={1000}>
+                    <Text style={styles.text_header}>Welcome to Recipe Finder!</Text>
+                </Animatable.View>
             </View>
-            <View style = {styles.footer}>
-                <Text style={styles.text_footer}>Email</Text>
+            <Animatable.View style = {styles.footer} animation = "fadeInUpBig" duration ={1000}>
+                <Text style={styles.text_footer}>Username</Text>
                 <View style={styles.action}>
                     <FontAwesome
                         name = "user-o" 
@@ -61,21 +116,27 @@ const SignInScreen = () => {
                         size = {20}
                     />
                     <TextInput
-                        placeholder = "Your Email"
+                        placeholder = "Your Username"
                         style = {styles.textInput}
                         autoCapitalize="none"
                         onChangeText={(val)=>textInputChange(val)}
+                        onEndEditing={(e)=>handleValidUser(e.nativeEvent.text)}
                     />
-                    {data.email.length > 0 &&
+                    {data.username.length > 0 &&
                     <Animatable.View 
-                        animation = 'bounceIn' >
+                        animation = 'bounceIn'>
                         <Feather
                         name = "check-circle"
-                        corlor = "purple"
+                        color = "purple"
                         size = {20} 
                         />
                     </Animatable.View>}
                 </View>
+                { data.isValidUser ? null : 
+                <Animatable.View animation="fadeInLeft" duration={500}>
+                    <Text style={styles.errorMsg}>Username must be at least 4 characters long.</Text>
+                </Animatable.View>
+                }
                 <Text style={[styles.text_footer, {marginTop: 35}]}>Password</Text>
                 <View style={styles.action}>
                     <FontAwesome
@@ -96,21 +157,27 @@ const SignInScreen = () => {
                         {data.secureTextEntry ?
                         <Feather
                             name = "eye-off"
-                            corlor = "grey"
+                            color = "grey"
                             size = {20}
                         />
                         :
                         <Feather
                             name = "eye"
-                            corlor = "grey"
+                            color = "grey"
                             size = {20}
                         />
                         }
                     </TouchableOpacity>
                 </View>
+                { data.isValidPassword ? null : 
+                <Animatable.View animation="fadeInLeft" duration={500}>
+                    <Text style={styles.errorMsg}>Password must be at least 8 characters long</Text>
+                </Animatable.View>
+                }
                 <View style = {styles.button}>
                     <TouchableOpacity
-                        onPress ={()=>alert('click')}
+                        style={styles.signIn}
+                        onPress ={()=>(loginHandle(data.username, data.password))}
                     >
                         <LinearGradient
                         colors = {['#8a2be2', '#483d8b']}
@@ -121,10 +188,22 @@ const SignInScreen = () => {
                         </LinearGradient>
                     </TouchableOpacity>
                 </View> 
-            </View>
+                <View style = {styles.button}>
+                    <TouchableOpacity
+                        onPress ={()=>navigation.navigate('SignUpScreen')}
+                    >
+                        <LinearGradient
+                        colors = {['#f0f8ff', '#f0ffff']}
+                        style={styles.signIn}
+                    >
+                        <Text style = {[styles.textSign, {
+                            color:'#8a2be2'}]}>Create account</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </View> 
+            </Animatable.View>
+            </KeyboardAwareScrollView>
         </View>
-
-     
     );
 }
 
@@ -133,13 +212,13 @@ export default SignInScreen;
 const styles = StyleSheet.create({
     container: {
       flex: 1, 
-      backgroundColor: '#8a2be2'
+      backgroundColor: '#8a2be2',
     },
     header: {
         flex: 1,
         justifyContent: 'flex-end',
         paddingHorizontal: 20,
-        paddingBottom: 50
+        paddingBottom: 50,
     },
     footer: {
         flex: 3,
@@ -152,7 +231,8 @@ const styles = StyleSheet.create({
     text_header: {
         color: '#fff',
         fontWeight: 'bold',
-        fontSize: 30
+        fontSize: 28,
+        textAlign: 'center'
     },
     text_footer: {
         color: '#05375a',
@@ -184,14 +264,14 @@ const styles = StyleSheet.create({
     },
     button: {
         alignItems: 'center',
-        marginTop: 50
+        marginTop: 45, 
     },
     signIn: {
         width: 350,
         height: 60,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 10
+        borderRadius: 10, 
     },
     textSign: {
         fontSize: 18,
