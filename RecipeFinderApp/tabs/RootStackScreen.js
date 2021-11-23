4b6b8b8bd7e99f85b82655f9e1c67d0b15827300
Ana Saldana <ana633@umail.ucsb.edu>
2021-11-23 14:11:59 -0800

    remove headers

--- RecipeFinderApp/tabs/RootStackScreen.js
+++ RecipeFinderApp/tabs/RootStackScreen.js
@@ -9,7 +9,7 @@ const RootStack = createStackNavigator();
 
 const RootStackScreen = ({navigation}) => {
     return (
-        <RootStack.Navigator >
+        <RootStack.Navigator  screenOptions={{headerShown: false}} >
             <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
             <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
             <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
